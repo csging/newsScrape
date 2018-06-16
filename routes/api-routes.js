ba2dfbce312b44bdf4a2f2a39199d99104afa7b4
Christian Gingrich <xian777@gmail.com>
2018-06-15 19:28:21 -0700

    now it views

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,72 +1,72 @@
-var request = require("request");
-var cheerio = require("cheerio");
-var Note = require("../models/Note.js");
-var Article = require("../models/Article.js");
-let articles = [];
+// var request = require("request");
+// var cheerio = require("cheerio");
+// var Note = require("../models/Note.js");
+// var Article = require("../models/Article.js");
+// let articles = [];
 
-module.exports = function(app) {
-    app.get("/", function(req, res) {
-      res.render("main", { articles });
-    });
+// module.exports = function(app) {
+//     app.get("/", function(req, res) {
+//       res.render("main", { articles });
+//     });
 
-app.get("/", function(req, res){ 
-    request('https://news.ycombinator.com', function (error, response, html){
-      if(!error && response.statusCode == 200) {
-        var $ = cheerio.load(html);
-        var results = [];
-        $('span.comhead').each(function(i, element){
-          var a = $(this).prev();
-          results.title = a.text();
-          results.url = a.attr('href');
-          var subtext = a.parent().parent().next().children('.subtext').children();
-          results.time = $(subtext).eq(2).text();
-          results.author = $(subtext).eq(1).text();
-          var metadata = {
-            title: results.title,
-            url: results.url,
-            time: "posted: " + results.time,
-            author: "by " + results.author
-          }
-          db.Article.create(metadata)
-            .then(function(dbArticle){
-              console.log(dbArticle); // <----------------------------
-            })
-            .catch(function(err){
-              return res.json(err);
-            });
-        // console.log(Article);
-        });
-        console.log("SCRAPE COMPLETE");
-      }
-    });
-    });
+// app.get("/", function(req, res){ 
+//     request('https://news.ycombinator.com', function (error, response, html){
+//       if(!error && response.statusCode == 200) {
+//         var $ = cheerio.load(html);
+//         var results = [];
+//         $('span.comhead').each(function(i, element){
+//           var a = $(this).prev();
+//           results.title = a.text();
+//           results.url = a.attr('href');
+//           var subtext = a.parent().parent().next().children('.subtext').children();
+//           results.time = $(subtext).eq(2).text();
+//           results.author = $(subtext).eq(1).text();
+//           var metadata = {
+//             title: results.title,
+//             url: results.url,
+//             time: "posted: " + results.time,
+//             author: "by " + results.author
+//           }
+//           db.Article.create(metadata)
+//             .then(function(dbArticle){
+//               console.log(dbArticle); // <----------------------------
+//             })
+//             .catch(function(err){
+//               return res.json(err);
+//             });
+//         // console.log(Article);
+//         });
+//         console.log("SCRAPE COMPLETE");
+//       }
+//     });
+//     });
     
-    app.get("/articles", function(req, res) {
-      db.Article.find({})
-        .then(function(dbArticle) {
-          res.json(dbArticle);
-        })
-        .catch(function(err) {
-          res.json(err);
-        });
-    });
+//     app.get("/articles", function(req, res) {
+//       db.Article.find({})
+//         .then(function(dbArticle) {
+//           res.json(dbArticle);
+//         })
+//         .catch(function(err) {
+//           res.json(err);
+//         });
+//     });
     
-    // Route for saving/updating an Article's associated Note
-    app.post("/articles/:id", function(req, res) {
-      // Create a new note and pass the req.body to the entry
-      db.Note.create(req.body)
-        .then(function(dbNote) {
-          // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-          // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-          // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-          return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
-        })
-        .then(function(dbArticle) {
-          res.json(dbArticle);
-        })
-        .catch(function(err) {
-          res.json(err);
-        });
-    });
+//     // Route for saving/updating an Article's associated Note
+//     app.post("/articles/:id", function(req, res) {
+//       // Create a new note and pass the req.body to the entry
+//       db.Note.create(req.body)
+//         .then(function(dbNote) {
+//           // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+//           // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+//           // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+//           return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+//         })
+//         .then(function(dbArticle) {
+//           res.json(dbArticle);
+//         })
+//         .catch(function(err) {
+//           res.json(err);
+//         });
+//     });
 
-    };
+//     };
--- server.js
+++ server.js
@@ -7,13 +7,12 @@ var mongoose = require("mongoose");
 var request = require("request");
 var cheerio = require ("cheerio");
 
-var PORT = 3000;
+var PORT =  process.env.PORT || 3000;
 var db = require("./models");
 
 var exphbs = require('express-handlebars');
-app.engine('handlebars', exphbs({
-  defaultLayout: 'main'
-}));
+
+app.engine('handlebars', exphbs({defaultLayout: "main"}));
 
 app.set('view engine', 'handlebars');
 app.use(logger("dev"));
@@ -23,56 +22,60 @@ app.use(bodyParser.json());
 
 mongoose.connect("mongodb://localhost/scrapedNews");
 
-app.get("/scrape", function(req, res){ 
+app.get("/", function(req, res){ 
 request('https://news.ycombinator.com', function (error, response, html){
   if(!error && response.statusCode == 200) {
     var $ = cheerio.load(html);
     var results = [];
     $('span.comhead').each(function(i, element){
+      
+      var articles = {};
       var a = $(this).prev();
-      results.title = a.text();
-      results.url = a.attr('href');
+      articles.title = a.text();
+      articles.url = a.attr('href');
       var subtext = a.parent().parent().next().children('.subtext').children();
-      results.time = $(subtext).eq(2).text();
-      results.author = $(subtext).eq(1).text();
+      articles.time = $(subtext).eq(2).text();
+      articles.author = $(subtext).eq(1).text();
       var metadata = {
-        title: results.title,
-        url: results.url,
-        time: "posted: " + results.time,
-        author: "by " + results.author
+        title: articles.title,
+        url: articles.url,
+        time: "posted: " + articles.time,
+        author: "by " + articles.author
       }
+      
       db.Article.create(metadata)
         .then(function(dbArticle){
-          console.log(dbArticle); // <----------------------------
+          res.render("main", dbArticle )
+          console.log("Scraping articles..."); // <----------------------------
         })
         .catch(function(err){
           return res.json(err);
         });
     // console.log(Article);
+    // results.push(articles);
     });
     console.log("SCRAPE COMPLETE");
   }
+//  res.render("main", articles )
 });
+// var dbArticle = db.Article;
+// res.render("article", dbArticle );
 });
 
 app.get("/articles", function(req, res) {
   db.Article.find({})
     .then(function(dbArticle) {
-      res.json(dbArticle);
+      res.render("articles", dbArticle )
     })
     .catch(function(err) {
       res.json(err);
     });
 });
 
-// Route for saving/updating an Article's associated Note
 app.post("/articles/:id", function(req, res) {
-  // Create a new note and pass the req.body to the entry
   db.Note.create(req.body)
     .then(function(dbNote) {
-      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+      
       return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
     })
     .then(function(dbArticle) {
@@ -83,7 +86,11 @@ app.post("/articles/:id", function(req, res) {
     });
 });
 
+// app.get("/", function (req, res){
+//   res.render
+// });
+
 
 app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+    console.log("App listening on http://localhost:" + PORT);
   });
