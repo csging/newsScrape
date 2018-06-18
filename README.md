575c6c7594eb317631860edc1950a42e30ad75c0
Christian Gingrich <xian777@gmail.com>
2018-06-18 09:58:27 -0700

    fixed some things, added to readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # newsScrape
+
+##  a mongo New York Times news scraping website, optimized with Cheerio, Node, Express, Handlebars, morgan, and body-parser
+
+### I wanted to do this assignment using Handlebars instead of using React as a refresher of sorts. I've been doing tutorials and demos mainly in React since starting project 2, so I wanted to challenge myself and do this in Handlebars. I ran into trouble mainly with trying to get Handlebars to work correctly. I spent Thursday night, all day friday, and the rest of saturday trying to hack it out. As of now, the app wont run correctly. I'm not sure why. I'll try to fix it before y'all grade it!
--- scripts/scrape.js
+++ scripts/scrape.js
@@ -2,26 +2,47 @@ var request = require("request");
 var cheerio = require ("cheerio");
 
 var scrape = function (cb) {
-    request("http://nytimes.com", function(err, res, body){
-        var $ = cheerio.load(body);
-        var articles = [];
-        $(".theme-summary").each(function(i, element){
-            var head = $(this).children(".story-heading").text().trim();
-            var sum = $(this).children(".summary").text().trim();
+    request('https://news.ycombinator.com', function (error, response, html){
+      var $ = cheerio.load(html);
+      var articles = [];
+      $('span.comhead').each(function(i, element){
+        var a = $(this).prev();
+        var head = a.text();
+        var sum = a.attr('href');
+        if(head && sum){
+        var headNeat = head.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
+        var sumNeat = sum.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
+        var dataToAdd = {
+            headline: headNeat,
+            summary: sumNeat
+        };
 
-            if(head && sum){
-                var headNeat = head.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
-                var sumNeat = sum.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
-                var dataToAdd = {
-                    headline: headNeat,
-                    summary: sumNeat
-                };
+        articles.push(dataToAdd);
+    }
+});
+cb(articles);
+});
+    //nyt request
+    // request("http://nytimes.com", function(err, res, body){
+    //     var $ = cheerio.load(body);
+    //     var articles = [];
+    //     $(".theme-summary").each(function(i, element){
+    //         var head = $(this).children(".story-heading").text().trim();
+    //         var sum = $(this).children(".summary").text().trim();
 
-                articles.push(dataToAdd);
-            }
-        });
-        cb(articles);
-    });
+    //         if(head && sum){
+    //             var headNeat = head.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
+    //             var sumNeat = sum.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
+    //             var dataToAdd = {
+    //                 headline: headNeat,
+    //                 summary: sumNeat
+    //             };
+
+    //             articles.push(dataToAdd);
+    //         }
+    //     });
+    //     cb(articles);
+    // });
 };
 
 module.exports = scrape;
